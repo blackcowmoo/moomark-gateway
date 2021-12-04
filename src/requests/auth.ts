8a11724c8a16a8aa14400098476cfd134a0a6bbf
micalgenus <micalgenus@gmail.com>
2021-12-05 02:13:55 +0900

    add dummy test

--- src/requests/auth.ts
+++ src/requests/auth.ts
@@ -1,20 +1,27 @@
-import { generateSession } from '@/core/session';
 import { endpoints } from '@/core/config';
 import axios from 'axios';
 
 const authServerHost = endpoints.auth.endpoint;
+const authAxios = axios.create({ baseURL: authServerHost });
+
+const generateSession = async () => {
+  const result = await authAxios.get('/login/session');
+  console.log(result);
+  return result;
+};
 
 export const googleLogin = async (code) => {
-  const result = await axios.get(`${authServerHost}/login/oauth2/code/google`, {
+  await generateSession();
+  const result = await authAxios.get('/login/oauth2/code/google', {
     params: {
       code,
       scope: 'email profile https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
       authuser: 0,
       prompt: 'consent',
     },
-    headers: {
-      Cookie: generateSession(),
-    },
+    // headers: {
+    //   Cookie: generateSession(),
+    // },
   });
 
   console.log(JSON.stringify(result));
