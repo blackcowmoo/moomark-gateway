30181a2fe5c1a7e15b07d11acb0762c07ead3315
GyeongSu Han <micalgenus@gmail.com>
2021-12-04 22:55:24 +0900

    Add google / github dummy test

--- src/graphql/resolvers/auth.ts
+++ src/graphql/resolvers/auth.ts
@@ -1,5 +1,12 @@
 export const Mutation = {
   login: async (_, { type, code }) => {
-    return `login: ${type}, ${code}`;
+    switch (type.toLowerCase()) {
+      case 'google':
+        return 'google: ' + code;
+      case 'github':
+        return 'github: ' + code;
+      default:
+        return '';
+    }
   },
 };
