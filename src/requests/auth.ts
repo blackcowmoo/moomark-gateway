ace3cd1520239e1473c9d0e4952e8748046a68a6
micalgenus <micalgenus@gmail.com>
2021-12-04 23:44:59 +0900

    typo

--- src/requests/auth.ts
+++ src/requests/auth.ts
@@ -5,18 +5,18 @@ import axios from 'axios';
 const authServerHost = endpoints.auth.endpoint;
 
 export const googleLogin = async (code) => {
-    const result = await axios.get(`${authServerHost}/login/oauth2/code/google`, {
-        params: {
-            code,
-            scope: 'email profile https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
-            authuser: 0,
-            prompt: 'consent'
-        },
-        headers: {
-            Cookie: generateSession()
-        }
-    });
+  const result = await axios.get(`${authServerHost}/login/oauth2/code/google`, {
+    params: {
+      code,
+      scope: 'email profile https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
+      authuser: 0,
+      prompt: 'consent',
+    },
+    headers: {
+      Cookie: generateSession(),
+    },
+  });
 
-    console.log(JSON.stringify(result));
-    return result;
-}
+  console.log(JSON.stringify(result));
+  return result;
+};
