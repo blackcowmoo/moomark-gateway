87cf45afe4f144681f67238df422e86c8b0aa0b6
GyeongSu Han <micalgenus@gmail.com>
2022-01-01 00:53:16 +0900

    fix auth login

--- src/requests/auth.ts
+++ src/requests/auth.ts
@@ -8,38 +8,10 @@ const authAxios = axios.create({
   headers: { Accept: 'application/json' },
 });
 
-const generateSession = async () => {
-  const result = await authAxios.get('/login/session', {
-    headers: { Accept: '*/*' },
-    validateStatus: (status) => status === 302,
-    withCredentials: true,
-  });
-
-  const [session] = result.headers['set-cookie'];
-  return session.split(';')[0];
-};
-
-const parseSession = (cookie: string) => {
-  return cookie.split(';')[0].split('=')[1];
-};
-
 export const googleLogin = async (state: string, code: string) => {
-  const session = await generateSession();
-  const result = await authAxios.get('/login/oauth2/code/google', {
-    params: {
-      state,
-      code,
-      scope: 'email profile https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
-      authuser: 0,
-      prompt: 'consent',
-    },
-    headers: {
-      Cookie: session,
-      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    },
-    validateStatus: (status) => status === 302,
-  });
+  const result = await authAxios.post('/api/v1/login/google', { token: code });
 
   console.log(JSON.stringify(result.headers));
+  console.log(JSON.stringify(result.data));
   return parseSession(session);
 };
