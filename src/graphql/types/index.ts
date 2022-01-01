9d91e6d81ef1945ca1ba91882f5d997a4e20f545
GyeongSu Han <micalgenus@gmail.com>
2022-01-02 00:49:44 +0900

    Add test route mutation (#4)
    * Add test route mutation
    
    * fix mutation?
    
    * Fix headers
    
    * Only dev

--- src/graphql/types/index.ts
+++ src/graphql/types/index.ts
@@ -1,11 +1,18 @@
 import { gql } from 'apollo-server-express';
-import auth from './auth';
+// import auth from './auth';
 
-const rootSchema = gql`
+const rootQuerySchema = gql`
   type Query {
     healthz: String!
     servers: String!
   }
 `;
 
-export const typeDefs = [rootSchema, auth];
+const rootMutationSchema = gql`
+  type Mutation {
+    request(method: String!, service: String!, url: String!, body: String, headers: String, params: String): String!
+    login(type: String!, code: String!, state: String): String!
+  }
+`;
+
+export const typeDefs = [rootQuerySchema, rootMutationSchema];
