c6f4e8af2519b8b5f9244601358e8d89f21a555d
micalgenus <micalgenus@gmail.com>
2022-03-20 21:16:59 +0900

    fix scope

--- src/requests/auth.ts
+++ src/requests/auth.ts
@@ -28,7 +28,7 @@ export const googleLogin = async (state: string, code: string, routes: { [KEY: s
     params: {
       state,
       code,
-      scope: 'email profile https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
+      scope: 'openid profile email',
     },
     headers: routes,
     // headers: {
