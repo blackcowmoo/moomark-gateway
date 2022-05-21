e3664e49065e2877dd9c89b8482f183965bcb75f
micalgenus <micalgenus@gmail.com>
2022-05-22 01:29:01 +0900

    add withdraw

--- src/test/mutation/login.test.ts
+++ src/test/mutation/login.test.ts
@@ -3,6 +3,13 @@ import { graphqlRequest, graphql } from '@/test/utils/app';
 import { testCode } from '@/test/utils/user';
 
 describe('GraphQL', () => {
+  let code = null;
+  let token = null;
+
+  before(() => {
+    code = testCode();
+  });
+
   it('Login', async () => {
     const query = graphql`
       mutation Login($code: String!) {
@@ -21,16 +28,31 @@ describe('GraphQL', () => {
       }
     `;
 
-    const { status, body } = await graphqlRequest(query, { variables: { code: testCode } });
+    const { status, body } = await graphqlRequest(query, { variables: { code } });
 
     assert.equal(status, 200);
     assert.isNotEmpty(body.data.login.token);
     assert.isNotEmpty(body.data.login.refreshToken);
-    assert.equal(body.data.login.user.id, `TEST@${testCode.split('-')[1]}`);
+    assert.equal(body.data.login.user.id, `TEST@${code.split('-')[1]}`);
     assert.equal(body.data.login.user.name, 'test');
     assert.equal(body.data.login.user.email, 'test@blackcowmoo.com');
     assert.equal(body.data.login.user.nickname, 'test');
     assert.equal(body.data.login.user.picture, 'https://www.gravatar.com/avatar/HASH');
     assert.equal(body.data.login.user.role, 'USER');
+
+    token = body.data.login.token;
+  });
+
+  after(async () => {
+    const query = graphql`
+      mutation {
+        withdraw
+      }
+    `;
+
+    const { status, body } = await graphqlRequest(query, { variables: { code }, headers: { Authorization: token } });
+
+    assert.equal(status, 200);
+    assert.isTrue(body.data.withdraw);
   });
 });
