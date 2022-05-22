ea740c75d259aa17a9a80d4b178b12128d7e134b
micalgenus <micalgenus@gmail.com>
2022-05-23 01:03:00 +0900

    add me test

--- src/test/mutation/login.test.ts
+++ src/test/mutation/login.test.ts
@@ -6,11 +6,9 @@ describe('GraphQL', () => {
   let code = null;
   let token = null;
 
-  before(() => {
+  before(async () => {
     code = generateTestCode();
-  });
 
-  it('Login', async () => {
     const query = graphql`
       mutation Login($code: String!) {
         login(type: "Google", code: $code) {
@@ -43,6 +41,31 @@ describe('GraphQL', () => {
     token = body.data.login.token;
   });
 
+  it('Me', async () => {
+    const query = graphql`
+      {
+        me {
+          id
+          name
+          email
+          nickname
+          picture
+          role
+        }
+      }
+    `;
+
+    const { status, body } = await graphqlRequest(query, { headers: { Authorization: token } });
+
+    assert.equal(status, 200);
+    assert.equal(body.data.me.id, `TEST@${code.split('-')[1]}`);
+    assert.equal(body.data.me.name, 'test');
+    assert.equal(body.data.me.email, 'test@blackcowmoo.com');
+    assert.equal(body.data.me.nickname, 'test');
+    assert.equal(body.data.me.picture, 'https://www.gravatar.com/avatar/HASH');
+    assert.equal(body.data.me.role, 'USER');
+  });
+
   after(async () => {
     const query = graphql`
       mutation {
@@ -50,7 +73,7 @@ describe('GraphQL', () => {
       }
     `;
 
-    const { status, body } = await graphqlRequest(query, { variables: { code }, headers: { Authorization: token } });
+    const { status, body } = await graphqlRequest(query, { headers: { Authorization: token } });
 
     assert.equal(status, 200);
     assert.isTrue(body.data.withdraw);
